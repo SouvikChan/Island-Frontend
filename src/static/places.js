f93c878dae961ca6cce75bb814b49dd1fc279198
Souvik Chandra <89825678+SouvikChan@users.noreply.github.com>
2022-03-03 12:45:09 +0530

    Add files via upload

---
+++ src/static/places.js
@@ -0,0 +1,32 @@
+const places = [
+  {
+    title: 'Green',
+    description:
+      "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Bora Bora',
+    description:
+      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Iziba',
+    description:
+      'Ibiza is one of the Balearic islands, an archipelago of Spain in the Mediterranean Sea. Its well known for the lively nightlife in Ibiza Town and Sant Antoni, where major European nightclubs have summer outposts. Its also home to quiet villages, yoga retreats.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island3.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Hawai',
+    description:
+      'The Big Island (officially named Hawaii) is the largest island in the United States Hawaiian archipelago in the Central Pacific. Its diverse terrain spans colored-sand beaches at Papakolea (green) and Punalu (black) to lush rainforest. Within Volcanoes National Park.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island4.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
